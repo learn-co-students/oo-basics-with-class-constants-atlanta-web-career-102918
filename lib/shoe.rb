a372b7fef158f67258890ad4c54cf934d06e4d0f
github username <github email address>
2018-11-03 17:25:37 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,26 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+  ## Question: Why does this not work: BRANDS = [].uniq
+
   def initialize(brand)
     @brand = brand
+    if BRANDS.any? { |word| word == brand }
+      nil
+    else
+      BRANDS << @brand 
+    end
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  #
+  # def brand=(new_brand)
+  #   @brand = new_brand
+  #   BRANDS << new_brand
+  # end
 
 end
